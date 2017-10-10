deb23ffd2319be645c1e63a03bbd06052d3f771f
Aric <wangyuanqi518@163.com>
2017-10-10 11:02:52 +0800

    v-0.1.2

--- routes/newsup.js
+++ routes/newsup.js
@@ -2,10 +2,9 @@
 var router = require('express').Router();
 var AV = require('leanengine');
 var News = AV.Object.extend('News');
-var fs = require('fs');
 
 
-router.post('/', async function(req, res, next) {
+router.post('/', function(req, res, next) {
   console.log(req.body);
   var newsa = new News();
   newsa.set('title', req.body.title);
@@ -13,9 +12,7 @@ router.post('/', async function(req, res, next) {
   newsa.set('eauthor', req.body.eauthor);
   newsa.set('tips', req.body.tips);
   newsa.set('tag', req.body.tag);
-  var bodyContent = await addFile(req.body.textBody);
-
-  newsa.set('textBody', bodyContent);
+  newsa.set('textBody', req.body.textBody);
 
   newsa.save().then(function(suc) {
     console.log(bodyContent);
@@ -31,22 +28,6 @@ router.get('/',(req, res, next)=>{
   res.render('newsup');
 });
 
-
-function addFile(data){
-	var dataNew = data || 'mock';
-  //生成新的静态页
-  var d=new Date();
-  d=d.getTime();
-  var pathStr='./public/news/'+d+'.txt';//通常是根据某种算法算出来的不重复的文件名
-  fs.writeFile(pathStr,dataNew.toString(),(err)=>{
-      if(err){
-          console.log('文件写入失败'+err);
-      }else{
-          console.log('文件写入成功');
-      }
-  });
-  return pathStr.slice(1);
-}
 
 
 module.exports = router;
