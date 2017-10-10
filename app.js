9dfb90417ffbcb12631b8b881d15a04d7c0be066
Aric <wangyuanqi518@163.com>
2017-10-10 16:22:37 +0800

    v-0.2.0

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ require('./cloud');
 
 var app = express();
 
+
 // 设置模板引擎
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -31,6 +32,18 @@ app.enable('trust proxy');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+
+//设置跨域访问
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 
 app.get('/', function(req, res) {
   res.render('index', { currentTime: new Date() });
--- routes/api/newsup.js
+++ routes/api/newsup.js
@@ -4,7 +4,7 @@ var AV = require('leanengine');
 var News = AV.Object.extend('News');
 
 
-router.post('/', function(req, res, next) {
+router.post('/api/post', function(req, res, next) {
 
   var newsa = new News();
   newsa.set('title', req.body.title);
@@ -26,7 +26,7 @@ router.post('/', function(req, res, next) {
 
 
 // 查询 News 列表
-router.get('/',(req, res, next)=>{
+router.get('/api/get',(req, res, next)=>{
   var query = new AV.Query(News);
   query.descending('createdAt');
   query.find().then(function(results) {
@@ -39,6 +39,32 @@ router.get('/',(req, res, next)=>{
       next(err);
     }
   }).catch(next);
+});
+
+// 查询 News 单篇文章
+router.get('/api/single/:aid',(req, res, next)=>{
+  var aid = req.params.aid;
+  if (isNaN(aid)) {
+    res.send({'code': 0,'desc':'参数不对。'});
+  }else{
+    var query = new AV.Query(News);
+    query.descending('createdAt');
+    query.find().then(function(results) {
+      if (results[aid]) {
+        res.send({'stat':200 ,'results':results[aid]});
+      }else{
+        res.send({'stat':301 ,'desc':'数据未创建'});
+      }
+    }, function(err) {
+      if (err.code === 101) {
+        // 该错误的信息为：{ code: 101, message: 'Class or object doesn\'t exists.' }，说明 News 数据表还未创建，所以返回空的 News 列表。
+        res.send({'stat':-1,'desc':'数据未创建'});
+      } else {
+        next(err);
+      }
+    }).catch(next);
+
+  }
 });
 
 
